f7cd75894ad12da06e374c7503c9b37c9061205a
Rudenko_Darya <46627206+Oshutkova@users.noreply.github.com>
2022-11-11 20:25:11 +0300

    Update vvk_hello_world.py

--- vvk_hello_world.py
+++ vvk_hello_world.py
@@ -1,1 +1,6 @@
-print('hello world!')
+from transformers import pipeline
+
+classifier = pipeline("sentiment-analysis",   
+                      "blanchefort/rubert-base-cased-sentiment")
+
+classifier("Я обожаю инженерию машинного обучения!")
