cde949b16d0e8163a6ec573653b59f5b4f9ae8b5
vvkunitskiy <vvkunitskiy@gmail.com>
2022-11-11 21:34:21 +0300

    add print function

--- vvk_hello_world.py
+++ vvk_hello_world.py
@@ -3,7 +3,8 @@ from transformers import pipeline
 classifier = pipeline("sentiment-analysis",   
                       "blanchefort/rubert-base-cased-sentiment")
 
-classifier("Я обожаю инженерию машинного обучения!")
+print(classifier("Я обожаю инженерию машинного обучения!"))
+
 ## Тут была Даша
 
 # Комментарий от Куницкого закоммиченный через VS Code
