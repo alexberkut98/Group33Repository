cc56033d9f12191c01fcc5df9c1a999360bdb265
vvkunitskiy <116190795+vvkunitskiy@users.noreply.github.com>
2022-11-18 21:12:02 +0300

    Update streamlit_test_33group.py

--- streamlit_test_33group.py
+++ streamlit_test_33group.py
@@ -1,1 +1,14 @@
-new file
+import io
+import streamlit as st
+
+st.title("Анализ тональности текста")
+st.markdown("_Практическая_ **работа** группы 3.3")
+
+from transformers import pipeline
+
+classifier = pipeline("sentiment-analysis",   
+                      "blanchefort/rubert-base-cased-sentiment")
+
+text_for_assasment = st.text_area("Введите текст для оценки токсичности:", value="")
+
+if st.button("Оценить"): st.markdown(classifier(text_for_assasment))
