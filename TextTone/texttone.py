54af09fe2df399e670167e6c24575b1779a20afc
vvkunitskiy <116190795+vvkunitskiy@users.noreply.github.com>
2022-12-02 23:52:23 +0300

    Update texttone.py
    изначальный код "приложения" мог быть таким

--- TextTone/texttone.py
+++ TextTone/texttone.py
@@ -1,1 +1,17 @@
+print("Анализ тональности текста")
+print("Практическая работа группы 3.3")
 
+from transformers import pipeline
+
+classifier = pipeline("sentiment-analysis",   
+                      "blanchefort/rubert-base-cased-sentiment")
+
+text_input = input("Введите текст для оценки токсичности:")
+
+text_res = classifier(text_input)[0]
+if text_res["label"]=="NEGATIVE":
+  text_out = "Это плохое высказывание от какого-то душнилы"
+elif text_res["label"]=="POSITIVE":
+  text_out = "По нашему мнению это доброе высказывание от хорошего человека"
+else: text_out = "Чёрт знает что хотел сказать автор"
+print(text_out)
