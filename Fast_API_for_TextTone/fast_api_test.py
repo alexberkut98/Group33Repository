0ce3641693bf53ef00246983b3af846e03fceeb2
vvkunitskiy <116190795+vvkunitskiy@users.noreply.github.com>
2022-12-02 23:42:44 +0300

    Update fast_api_test.py
    fix stupid bugs

--- Fast_API_for_TextTone/fast_api_test.py
+++ Fast_API_for_TextTone/fast_api_test.py
@@ -2,29 +2,24 @@ from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
 
-class Text_for_chek(BaseModel):
+class Text_for_check(BaseModel):
     text: str
-
-#Анализ тональности текста
-#Практическая работа группы 3.3
-#Тест FastAPI приложения
 
 app = FastAPI()
 
+classifier = pipeline("sentiment-analysis",   
+                      "blanchefort/rubert-base-cased-sentiment")
+
 @app.get("/")
-def root();
-  return{"message": "
-Это практическая работа группы 3.3
-Анализ тональности текста
-Тест FastAPI приложения
-Отправьте текст для оценки токсичности на: <адрес>/chektext/ методом POST"}
+def root():
+  return{"message": "Это практическая работа группы 3.3. Анализ тональности текста. Тест FastAPI приложения. Отправьте текст для оценки токсичности на: <адрес>/chektext/ методом POST"}
 
 @app.post("/chektext/")
-def chektext(item: Text_for_chek):
-  text_res = classifier(Text_for_chek)[0]
+def chektext(item: Text_for_check):
+  text_res = classifier(item.text)[0]
   if text_res["label"]=="NEGATIVE":
     text_out = "Это плохое высказывание от какого-то душнилы"
   elif text_res["label"]=="POSITIVE":
     text_out = "По нашему мнению это доброе высказывание от хорошего человека"
   else: text_out = "Чёрт знает что хотел сказать автор"
-return text_out
+  return text_out
