960f4f093d91f6c2e63626c1896a5098f645e925
arqoofficial <argolubev97@yandex.ru>
2022-12-04 11:22:30 +0300

    добавил оценку высказывания в процентах

--- TextTone/texttone.py
+++ TextTone/texttone.py
@@ -6,12 +6,12 @@ from transformers import pipeline
 classifier = pipeline("sentiment-analysis",   
                       "blanchefort/rubert-base-cased-sentiment")
 
-text_input = input("Введите текст для оценки токсичности:")
+text_input = input("Введите текст для оценки токсичности:\t")
 
 text_res = classifier(text_input)[0]
 if text_res["label"]=="NEGATIVE":
-  text_out = "Это плохое высказывание от какого-то душнилы"
+  text_out = f"Это плохое высказывание от какого-то душнилы на {int(round(text_res['score']*100, 0))}%"
 elif text_res["label"]=="POSITIVE":
-  text_out = "По нашему мнению это доброе высказывание от хорошего человека"
+  text_out = f"По нашему мнению это доброе высказывание от хорошего человека на {int(round(text_res['score']*100, 0))}%"
 else: text_out = "Чёрт знает что хотел сказать автор"
 print(text_out)
