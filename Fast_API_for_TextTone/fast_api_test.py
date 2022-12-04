a87590984d5b464c478342cf9471bf6f77ec1dc3
arqoofficial <argolubev97@yandex.ru>
2022-12-04 15:45:51 +0300

    Изменил выдачу результатов

--- Fast_API_for_TextTone/fast_api_test.py
+++ Fast_API_for_TextTone/fast_api_test.py
@@ -12,14 +12,16 @@ classifier = pipeline("sentiment-analysis",
 
 @app.get("/")
 def root():
-  return{"message": "Это практическая работа группы 3.3. Анализ тональности текста. Тест FastAPI приложения. Отправьте текст для оценки токсичности на: <адрес>/chektext/ методом POST"}
+  return{"message": "Это практическая работа группы 33. Анализ тональности текста. Тест FastAPI приложения. Отправьте текст для оценки токсичности на: <адрес>/chektext/ методом POST"}
 
 @app.post("/chektext/")
 def chektext(item: Text_for_check):
   text_res = classifier(item.text)[0]
   if text_res["label"]=="NEGATIVE":
-    text_out = "Это плохое высказывание от какого-то душнилы"
+    text_out = f"По нашему мнению это негативное высказывание. \n Уровень достоверности = {int(round(text_res['score']*100, 0))}%"
   elif text_res["label"]=="POSITIVE":
-    text_out = "По нашему мнению это доброе высказывание от хорошего человека"
+    text_out = f"По нашему мнению это доброе высказывание. \n Уровень достоверности = {int(round(text_res['score']*100, 0))}%"
+  elif text_res["label"]=="NEUTRAL":
+    text_out = f"По нашему мнению это высказываение нейтрально. \n Уровень достоверности = {int(round(text_res['score']*100, 0))}%"
   else: text_out = "Чёрт знает что хотел сказать автор"
   return text_out
