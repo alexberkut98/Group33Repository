4be51ae80691557b1d63991c8d922c3557c5b6aa
arqoofficial <argolubev97@yandex.ru>
2022-12-04 19:02:38 +0300

    Немного подправил вывод

--- Fast_API_for_TextTone/fast_api_test.py
+++ Fast_API_for_TextTone/fast_api_test.py
@@ -18,10 +18,10 @@ def root():
 def chektext(item: Text_for_check):
   text_res = classifier(item.text)[0]
   if text_res["label"]=="NEGATIVE":
-    text_out = f"По нашему мнению это негативное высказывание. \n Уровень достоверности = {int(round(text_res['score']*100, 0))}%"
+    text_out = f"По нашему мнению это негативное высказывание. Уровень достоверности = {int(round(text_res['score']*100, 0))}%"
   elif text_res["label"]=="POSITIVE":
-    text_out = f"По нашему мнению это доброе высказывание. \n Уровень достоверности = {int(round(text_res['score']*100, 0))}%"
+    text_out = f"По нашему мнению это доброе высказывание. Уровень достоверности = {int(round(text_res['score']*100, 0))}%"
   elif text_res["label"]=="NEUTRAL":
-    text_out = f"По нашему мнению это высказываение нейтрально. \n Уровень достоверности = {int(round(text_res['score']*100, 0))}%"
+    text_out = f"По нашему мнению это высказываение нейтрально. Уровень достоверности = {int(round(text_res['score']*100, 0))}%"
   else: text_out = "Чёрт знает что хотел сказать автор"
   return text_out
