832bea54cec1b8c4c9cd21f9f2dfd6fb103e108d
arqoofficial <argolubev97@yandex.ru>
2022-12-09 20:23:02 +0300

    update streamlit_app

--- StreamLit_for_TextTone/streamlit_test_33group.py
+++ StreamLit_for_TextTone/streamlit_test_33group.py
@@ -1,18 +1,20 @@
 import io
 import streamlit as st
+from transformers import pipeline
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    return pipeline("sentiment-analysis",   
+                      "blanchefort/rubert-base-cased-sentiment")
+                      
+model = load_model()
 
 st.title("Анализ тональности текста")
 st.markdown("Практическая работа группы **33**")
-
-from transformers import pipeline
-
-classifier = pipeline("sentiment-analysis",   
-                      "blanchefort/rubert-base-cased-sentiment")
-
 text_input = st.text_area("Введите текст для оценки токсичности:", value="")
 
 if st.button("Оценить"):
-  text_res = classifier(text_input)[0]
+  text_res = model(text_input)[0]
   if text_res["label"]=="NEGATIVE":
     text_out = f"По нашему мнению это негативное высказывание. \n Уровень достоверности = {int(round(text_res['score']*100, 0))}%"
   elif text_res["label"]=="POSITIVE":
