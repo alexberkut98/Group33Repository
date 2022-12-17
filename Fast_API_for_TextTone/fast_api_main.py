252c0dedb279ace7f8b5f3793bc2a14a20d8d2cb
PismarovMikhail <120199397+PismarovMikhail@users.noreply.github.com>
2022-12-17 13:29:38 +0300

    Update fast_api_main.py
    Привел в соответствие с PIP8

--- Fast_API_for_TextTone/fast_api_main.py
+++ Fast_API_for_TextTone/fast_api_main.py
@@ -1,27 +1,41 @@
+# Connecting the FastAPI, pipeline and BaseModel classes from the 
+# fastapi, transformers and pydantic libraries
+
 from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
 
 class Text_for_check(BaseModel):
     text: str
-
+        
+# Creating a FastAPI object
 app = FastAPI()
 
+# Creating a classifier from the Hugging Face library
 classifier = pipeline("sentiment-analysis",   
                       "blanchefort/rubert-base-cased-sentiment")
 
 @app.get("/")
 def root():
-  return{"message": "Это практическая работа группы 33. Анализ тональности текста. Тест FastAPI приложения. Отправьте текст для оценки токсичности на: <адрес>/checktext/ методом POST"}
+"""Requests to the root directory of the server"""
+    return {"message": "Это практическая работа группы 33. Анализ тональности текста. Тест FastAPI приложения. Отправьте текст дляоценки токсичности на: <адрес>/checktext/ методом POST"}
 
 @app.post("/checktext/")
 def chektext(item: Text_for_check):
-  text_res = classifier(item.text)[0]
-  if text_res["label"]=="NEGATIVE":
-    text_out = f"По нашему мнению это негативное высказывание. Уровень достоверности = {int(round(text_res['score']*100, 0))}%"
-  elif text_res["label"]=="POSITIVE":
-    text_out = f"По нашему мнению это доброе высказывание. Уровень достоверности = {int(round(text_res['score']*100, 0))}%"
-  elif text_res["label"]=="NEUTRAL":
-    text_out = f"По нашему мнению это высказываение нейтрально. Уровень достоверности = {int(round(text_res['score']*100, 0))}%"
-  else: text_out = "Чёрт знает что хотел сказать автор"
-  return text_out
+""" Text analysis output """
+
+    text_res = classifier(item.text)[0]
+    
+    if text_res["label"] == "NEGATIVE":
+        text_out = f"По нашему мнению это негативное высказывание. Уровень достоверности = {int(round(text_res['score']*100, 0))}%"
+    
+    elif text_res["label"] == "POSITIVE":
+        text_out = f"По нашему мнению это доброе высказывание. Уровень достоверности = {int(round(text_res['score']*100, 0))}%"
+    
+    elif text_res["label"] == "NEUTRAL":
+        text_out = f"По нашему мнению это высказываение нейтрально. Уровень достоверности = {int(round(text_res['score']*100, 0))}%"
+    
+    else: 
+        text_out = "Не понятно что хотел сказать автор"
+    
+    return text_out
