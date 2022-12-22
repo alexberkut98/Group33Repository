e50ad20a2dbba5946f570ba34167b1d6c996cc66
arqoofficial <119440844+arqoofficial@users.noreply.github.com>
2022-12-22 19:51:30 +0300

    Update vvk_hello_world.py

--- vvk_hello_world.py
+++ vvk_hello_world.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 classifier = pipeline("sentiment-analysis",   
                       "blanchefort/rubert-base-cased-sentiment")
 
-print(classifier("Я обожаю инженерию машинного обучения!")
+print(classifier("Я обожаю инженерию машинного обучения!"))
 
 ## Тут была Даша
 
