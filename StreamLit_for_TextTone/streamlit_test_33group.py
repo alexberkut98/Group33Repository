d94e48e957c3e2a851b58ef9290dde58f8b511a5
arqoofficial <argolubev97@yandex.ru>
2023-04-02 20:28:13 +0300

    update according to PEP8

--- StreamLit_for_TextTone/streamlit_test_33group.py
+++ StreamLit_for_TextTone/streamlit_test_33group.py
@@ -1,25 +1,36 @@
-import io
 import streamlit as st
 from transformers import pipeline
 
+
 @st.cache(allow_output_mutation=True)
 def load_model():
-    return pipeline("sentiment-analysis",   
-                      "blanchefort/rubert-base-cased-sentiment")
-                      
+    return pipeline(
+        "sentiment-analysis",
+        "blanchefort/rubert-base-cased-sentiment"
+    )
+
+
 model = load_model()
 
 st.title("Анализ тональности текста")
 st.markdown("Практическая работа группы **33**")
-text_input = st.text_area("Введите текст для оценки токсичности:", value="")
+text_input = st.text_area(
+    "Введите текст для оценки токсичности:",
+    value=""
+)
 
 if st.button("Оценить"):
-  text_res = model(text_input)[0]
-  if text_res["label"]=="NEGATIVE":
-    text_out = f"По нашему мнению это негативное высказывание. \n Уровень достоверности = {int(round(text_res['score']*100, 0))}%"
-  elif text_res["label"]=="POSITIVE":
-    text_out = f"По нашему мнению это доброе высказывание. \n Уровень достоверности = {int(round(text_res['score']*100, 0))}%"
-  elif text_res["label"]=="NEUTRAL":
-    text_out = f"По нашему мнению это высказываение нейтрально. \n Уровень достоверности = {int(round(text_res['score']*100, 0))}%"
-  else: text_out = "Чёрт знает что хотел сказать автор"
-  st.header(text_out)
+    text_res = model(text_input)[0]
+    percent = int(round(text_res["score"]*100, 0))
+    if text_res["label"] == "NEGATIVE":
+        text_out = f"По нашему мнению это негативное высказывание.\n\
+                     Уровень достоверности = {percent}%"
+    elif text_res["label"] == "POSITIVE":
+        text_out = f"По нашему мнению это доброе высказывание.\n\
+                     Уровень достоверности = {percent}%"
+    elif text_res["label"] == "NEUTRAL":
+        text_out = f"По нашему мнению это высказываение нейтрально.\n\
+                     Уровень достоверности = {percent}%"
+    else:
+        text_out = "Чёрт знает что хотел сказать автор"
+    st.header(text_out)
